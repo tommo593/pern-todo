41053f67bb484578f18303aa219b81ba64ec174b
tommo593 <tomdevwork@gmail.com>
2024-09-21 23:53:01 +0100

    Added "update a todo"

--- server/index.js
+++ server/index.js
@@ -38,7 +38,33 @@ app.get("/todos", async (req, res) => {
 
 // get a todo
 
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
 // update a todo
+
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+    res.json("Todo was updated!");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 // delete a todo
 
