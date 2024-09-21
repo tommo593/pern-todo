83da3ec1c3b40742f36368632c37a2267cf18540
tommo593 <tomdevwork@gmail.com>
2024-09-21 23:38:08 +0100

    Added "get all todos"

--- server/index.js
+++ server/index.js
@@ -15,17 +15,26 @@ app.post("/todos", async (req, res) => {
   try {
     const { description } = req.body;
     const newTodo = await pool.query(
-      "INSERT INTO todo (description) VALUES($1)",
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
       [description]
     );
 
-    res.json(newTodo);
+    res.json(newTodo.rows[0]);
   } catch (err) {
     console.error(err.message);
   }
 });
 
 // get all todos
+
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 // get a todo
 
