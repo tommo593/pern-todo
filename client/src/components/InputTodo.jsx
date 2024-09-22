24a6d6a99164e84fe716fdd0cc47493e9950f9d0
tommo593 <tomdevwork@gmail.com>
2024-09-22 04:58:08 +0100

    Added stuff

---
+++ client/src/components/InputTodo.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const InputTodo = () => {
+  return (
+    <>
+      <h1 className="text-center mt-5">PERN Todo List</h1>
+      <form action="" className="d-flex align-items-center">
+        <input type="text" className="form_control" />
+        <button className="btn btn-success">Add</button>
+      </form>
+    </>
+  );
+};
+
+export default InputTodo;
